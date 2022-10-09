4bde7fb80bbbad927a0579a26fe2afe8879c8b33
dzianis danilkou <d.danilkov@dialmyapp.com>
2022-10-09 21:38:01 +0300

    wip task5

--- import-service/src/functions/importFileParser/handler.ts
+++ import-service/src/functions/importFileParser/handler.ts
@@ -1,48 +1,42 @@
 import { middyfy } from "@libs/lambda";
-import { S3Event } from "aws-lambda";
 import { s3Client } from "@libs/s3";
-
 const csv = require("csv-parser");
 
-const importFileParser = async (event: S3Event) => {
-  try {
-    const fileRecord = event.Records[0];
-    const key = fileRecord.s3.object.key;
-    const results = [];
+const BUCKET_NAME = "my-products-file-bucket";
 
-    const response = s3Client
-      .getObject({
-        Bucket: fileRecord.s3.bucket.name,
-        Key: fileRecord.s3.object.key,
-      })
-      .createReadStream();
+const importFileParser: any = async (event) => {
+  const promises = event.Records.map((record) => {
+    return new Promise(() => {
+      const params = { Bucket: BUCKET_NAME, Key: record.s3.object.key };
+      const file = s3Client.getObject(params).createReadStream();
 
-    return response
-      .pipe(csv())
-      .on("data", (data) => results.push(data))
-      .on("end", () => {
-        console.log("RESULTS", results);
+      file
+        .pipe(csv())
+        .on("data", (data) => {
+          console.log("LOG: Data row: ", data);
+        })
+        .on("end", async () => {
+          console.log("LOG: Copying the file: ", record.s3.object.key);
+          await s3Client
+            .copyObject({
+              Bucket: BUCKET_NAME,
+              CopySource: BUCKET_NAME + "/" + record.s3.object.key,
+              Key: record.s3.object.key.replace("uploaded", "parsed"),
+            })
+            .promise();
 
-        s3Client
-          .copyObject({
-            Bucket: fileRecord.s3.bucket.name,
-            CopySource: fileRecord.s3.bucket.name + "/" + key,
-            Key: key.replace("uploaded", "parsed"),
-          })
-          .promise()
-          .then(() => {
-            console.log("LOG: removing the file: ", fileRecord.s3.object.key);
-            s3Client
-              .deleteObject({
-                Bucket: fileRecord.s3.bucket.name,
-                Key: fileRecord.s3.object.key,
-              })
-              .promise();
-          });
-      });
-  } catch (error) {
-    console.log('ERROR', error.message);
-  }
+          console.log("LOG: Removing the file: ", record.s3.object.key);
+          await s3Client
+            .deleteObject({
+              Bucket: BUCKET_NAME,
+              Key: record.s3.object.key,
+            })
+            .promise();
+        });
+    });
+  });
+
+  return Promise.all(promises);
 };
 
 export const main = middyfy(importFileParser);
